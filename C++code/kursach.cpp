d30b275ced3f90a4e5a1765ec35dff7dcf16d4e2
Dmitriy Ogureckiy <41239838+odimon21@users.noreply.github.com>
2020-06-04 22:36:32 +0300

    II ver

--- C++code/kursach.cpp
+++ C++code/kursach.cpp
@@ -1,101 +1,125 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
 #include <string>
+#include <locale>
+#include <sstream>
+#include <cstdlib>
+
 
 using namespace std;
 
-
-class InputController
-{
+class InputStopChecker22 {
 private:
-	string chek1;//Последнее слово предыдущей строки
-	string chek2;//Последнее слово настоящей строки
+	locale loc;      //
+	int numeral_sum; // сумма цифр в строке
 public:
-	int kolvo;//финальное кол-во строк
-	int strl[25];//длинна строки
-	string razdelitel[1];//символы разделители
-	string str[25];//массив строк
-
-
-
-void get_str()//метод ввода строк + условие остановки ввода
-{
-	cout << "Введите текст\n";
-	for (int i = 0; i < 25; i++)
+	InputStopChecker22() 
 	{
-		cout << "Введите " << i + 1 << " строку \n";
-		getline(cin, str[i]);
-		strl[i] = str[i].length();
-		if (strl[i] > 100)
-		{
-			cout << "Введено более 100 символов, введи строку еще раз." << endl;
-			i--;
-			continue;
-		}
-
-
+		numeral_sum = 0;
 	}
-}
-	
-void get_razdelitel()//метод ввода разделителей
-{
-	int razdelitel_l;// кол-во разделителей
-	cout << "Введите разделитель:" << endl;
-	char a;
-	int probel = 0;// пробел на клавиатуре
-	for (int i = 0; i < 5; i++)
+	bool checkStopCondition() 
 	{
-
-		cout << i + 1 << ")=";
-		cin >> razdelitel[0][i];
-		
-		cout << "\nпрекратить ввод разделителей ? (1 — да, нет — любой символ) ";
-		cin >> a;
-		cout << endl;
-		if (a == '1')
+		if (numeral_sum > 100) {return true;}
+		else 
 		{
-			break;
+			return false;
 		}
 	}
-	razdelitel_l = razdelitel[0].length();
-	cout << "Количество разделителей " << razdelitel_l << endl;
+	void handleLine(string & line) {
+		for (int i = 0; i < line.size(); i++) {
+			if ('0' <= line[i] && line[i] <= '9') {
+				int digit = line[i] - '0';
+				numeral_sum += digit;
+			}
+		}
+	}
+};
+
+class InputHandler23 {
+private:
+	int simbol_counter;
+	string output; 
+public:
+	InputHandler23() {
+		simbol_counter = 0;
+		output = "";
+	}
+	void handleLine(string & line) {
+		if (simbol_counter < 100) {
+			for (int i = 0; i < line.size(); i++) {
+				if (simbol_counter < 100) {
+					if (line[i] != ' ' && line[i] != '\n' && line[i] != '\t') {
+						simbol_counter++;
+					}
+					
+					output += line[i];
+				}
+				else {
+					break;
+				}
+			}
+			output += '\n';
+		}
+	}
+	string getOutput() { return output; }
+};
+
+class ConsoleOutputHandler {
+public:
+	void handleOutput(string & output) {
+		cout << output;
+	}
+};
+
+void handleInput(istream & istream, ostream & ostream, InputStopChecker22* checker, InputHandler23* handler, ConsoleOutputHandler* output) {
+	cout << "===Please enter no more than 25 string with length no more than 100." << endl;
+	bool stopped = false;
+	int line_counter = 0;
+	while (!stopped) {
+		string line;
+		getline(istream, line);
+		cout << "Length of line : " << line.length() << endl;
+		if (line.length() > 100) {
+			ostream << "===Your line have length more than 100 character it will be resised to maximal length 100." << endl;
+			line.resize(100);
+		}
+		handler->handleLine(line);
+		checker->handleLine(line);
+		line_counter++;
+		if (checker->checkStopCondition()) {
+			ostream << "===Sum of numbers in your input more than 100 input stopped." << endl;
+			stopped = true;
+		}
+		if (!stopped && line_counter == 25) {
+			ostream << "===You entered 25 strings. Input stopped." << endl;
+			stopped = true;
+		}
+	}
+	ostream << "===Start output." << endl;
+	string out = handler->getOutput();
+	cout << "Length of line : " << out.length() << endl;
+	output->handleOutput(out);
+	ostream << "===Output finished." << endl;
 }
 
-	
-};
+int main() {
 
-class TextProcessor
-{
-	
-};
-		
-		
+	InputStopChecker22 checker;
+	InputHandler23 handler;
+	ConsoleOutputHandler output;
 
-
-int main()
-{
-	setlocale(LC_ALL, "Russian");
-	InputController *vvod = new InputController;
-	/*vvod->get_str();
-	delete vvod;*/
-	vvod->get_razdelitel();
-	
-	
-	_getch();
+	handleInput(cin, cout, &checker, &handler, &output);
+	system("pause");
 	return 0;
 }
 
 
-		
-	
-		
-	
-	
 
 	
 	
-		
-	
-	
-				
-	
+
+
+
+
+
+
+
